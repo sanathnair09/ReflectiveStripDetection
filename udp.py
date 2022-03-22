59ffec6f663f4d026a6e374a4c528e398626d94d
Sanath Nair <sanathnair09@gmail.com>
2022-03-21 17:04:46 -0700

    code

--- udp.py
+++ udp.py
@@ -4,32 +4,19 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-BBOX_COLOR = (227, 5, 216)
-
-AREA_THRESHOLD = 400
-RADIUS = 10
-COLOR = (255, 0, 0)
-THICKNESS = -1
-
-FRAME_WIDTH = cap.get(3)  # float `width`
-FRAME_HEIGHT = cap.get(4)  # float `height`
-
-
-# focal length = (px_width * distance_from_camera) / object_width
-
-# the color lol
 HUE = 100
 # HUE threshold (how much above and below the actual color we are trying to detect)
 THRESHOLD = 25
 
-lower_green = np.array([HUE-THRESHOLD, 100, 100])
-upper_green = np.array([HUE+THRESHOLD, 225, 255])
+LOWER_GREEN = np.array([HUE-THRESHOLD, 100, 100])
+UPPER_GREEN = np.array([HUE+THRESHOLD, 225, 255])
 
-UDP_ID = "10.40.79.2"
-UDP_PORT=5805
+AREA_THRESHOLD = 500
+
+UDP_IP = "localhost"
+UDP_PORT = 8080
 
 while True:
-    # get frame from camera
     _, frame = cap.read()
     # convert the frame from BGR to HSV
     # opencv2 uses BGR instead of RGB because of some stupid reason
@@ -44,7 +31,7 @@ while True:
     # try and keep satuaration and values as low as possible to remove noise although increasing the threshold will allow for higher chance of detection
 
     # using the lower and upper HSV thresholds and filtering out whatever doesn't fall in the threshold
-    mask = cv2.inRange(hsv, lower_green, upper_green)
+    mask = cv2.inRange(hsv, LOWER_GREEN, UPPER_GREEN)
 
     # (OPTIONAL) blurred mask
     # mask_blurred = cv2.inRange(hsv_blurred, lower_green, upper_green)
@@ -65,34 +52,15 @@ while True:
                 M = cv2.moments(cntr)
                 cX += int(M["m10"] / M["m00"])
                 cY += int(M["m01"] / M["m00"])
-                # rect = cv2.minAreaRect(cntr)
-                # box = cv2.boxPoints(rect)
-                # box = np.int0(box)  # box[0] bottom most point
-                # print(box[:, 0])
-                # print(np.max(box[:, 0]) - np.min(box[:, 0]))
-
-                # cv2.circle(mask, [box[0][0], box[0][1]], RADIUS, BBOX_COLOR, 5)
-                # cv2.circle(mask, [box[1][0], box[1][1]], RADIUS, BBOX_COLOR, 2)
-
-                # cv2.drawContours(mask, [box], 0, BBOX_COLOR, 4)
+            
 
     if len(filtered_contours) > 1:
         cX = int(cX/len(filtered_contours))
         cY = int(cY/len(filtered_contours))
         center_coordinates = (cX, cY)
         print(center_coordinates)
-        # Using cv22.circle() method
-        # cv2.circle(mask, center_coordinates, RADIUS, COLOR, THICKNESS)
 
-    # showing what camera sees and output of HSV filtered frames
-    # cv2.imshow("original", frame)
-    # cv2.imshow("hsv", hsv)
-
-    cv2.imshow("filtered", mask)
+    MESSAGE=f"{cX}:{cY}"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes(MESSAGE), (UDP_IP, UDP_PORT))
     cX, cY = 0, 0
-    # kill if escape is pressed
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-
-cv2.destroyAllWindows()
