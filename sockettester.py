26cd6396000500273128cc1969b4d3fb6b513d4b
Sanath Nair <sanathnair09@gmail.com>
2022-03-23 01:51:22 -0700

    works

--- sockettester.py
+++ sockettester.py
@@ -1,10 +1,15 @@
+from random import Random
 import socket
 import threading
 
-SEND_IP = "192.168.1.69"
-RECEIVE_IP = socket.gethostbyname(socket.gethostname())
-print(RECEIVE_IP)
-UDP_PORT = 8080
+SEND_IP = "192.168.1.69"  # roborio IP
+RECEIVE_IP = "192.168.1.18"  # nano IP
+UDP_PORT = 8080  # port
+
+
+def getAngleToTurn():
+    angle = Random.randint(0, 120)
+    return str(angle)
 
 
 def listen_to_RIO():
@@ -14,21 +19,19 @@ def listen_to_RIO():
     while(True):
         data, addr = sock.recvfrom(2048)
         print("data: " + data)
-        print("addr: " + addr)
+        print("addr: " + str(addr))
         msg = data
         if(msg == "GET"):
-            break
+            # def get coords
+            angle = getAngleToTurn()
+            send_to_RIO(angle)
 
 
 def send_to_RIO(message: str):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(bytes(message), (SEND_IP, UDP_PORT))
-    print("sending data: " + bytes(message))
+    print("sent data")
 
 
-def __main__():
-    listenThread = threading.Thread(target=listen_to_RIO)
-    sendThread = threading.Thread(target=send_to_RIO, args=["hi from nano"])
-    listenThread.start()
-    sendThread.start()
-    print("done")
+while(True):
+    listen_to_RIO()
